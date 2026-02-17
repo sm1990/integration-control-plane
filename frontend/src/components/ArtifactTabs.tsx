2fff276d7b5e798d9a6a9173ddda710113286cfc
Sarani Mendis <saranimendisb@gmail.com>
2026-02-17 09:46:42 +0530

    Add license headers

--- frontend/src/components/ArtifactTabs.tsx
+++ frontend/src/components/ArtifactTabs.tsx
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import { Box, Chip, CircularProgress, Stack, Typography } from '@wso2/oxygen-ui';
 import { useArtifactSource, useLocalEntryValue, ARTIFACT_TYPE_TO_SOURCE_TYPE } from '../api/queries';
 import CodeViewer from './CodeViewer';
